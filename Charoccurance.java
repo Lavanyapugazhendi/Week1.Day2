5b1765dfb7f054b8c2a54e7a7a3867902e02bab4
Lavanyapugazhendi <88805434+Lavanyapugazhendi@users.noreply.github.com>
2021-08-16 00:11:44 +0530

    Add files via upload

---
+++ Charoccurance.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class Charoccurance {
+
+	public static void main(String[] args) {
+		String str = "Happy day";
+		int character = 0;
+		char[] charArray = str.toCharArray();
+		int str1 =charArray.length;
+		for (int i = 0; i < charArray.length; i++) {
+			if (charArray[i] == 'y') {
+				character++;
+				
+				
+			}
+			
+		}
+		System.out.println(character);
+
+	}
+
+}
